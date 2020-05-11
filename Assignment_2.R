edb5979b8e10fdcab4fbe01464da2f9fa3021861
Michael Allouche <michael.allouche@student-cs.fr>
2020-05-11 21:39:09 +0200

    Question 1 + graphs

--- Assignment_2.R
+++ Assignment_2.R
@@ -10,17 +10,27 @@ lambda_exp = 1/mu
 
 set.seed(124)
 
-data_norm = rnorm(n, mu, sd)           # generating random numbers from normal distribution
+data_norm = rnorm(n, mu, sd)          # generating random numbers from normal distribution
 data_exp = rexp(n,lambda_exp)          # generating random numbers from exponential distribution
+
+hist(data_norm, 
+     main = "Histogram of Normal Distribution", 
+     xlab = "Normal Distribution",
+     col="grey",
+     breaks=50,
+     prob = TRUE)
+
+hist(data_exp, 
+     main = "Histogram of Exponential Distribution", 
+     xlab = "Exponential Distribution",
+     col="grey",
+     breaks=50,
+     prob = TRUE)
 
 ### QUESTION 2 ###
 # Simulate the probability density function of the exponential sample for n = 5, 50, 500, 5000, 10000
 # What do you observe for the empirical mean ?
 
-
-dnorm(data_norm,mu_norm,sd_norm)
-
-
-
-
+lambda = 10
+n_list = c(5, 50 , 500, 5000, 10000)
 
